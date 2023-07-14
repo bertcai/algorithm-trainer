66026ec8fa309312afbf7c9d97e1fa51634bb1f8
bert_cai <bert_cai@outlook.com>
2023-07-14 11:01:05 +0800

    leetcode: solution 3, better version

--- leetcode/3.longest-substring-without-repeating-characters.ts
+++ leetcode/3.longest-substring-without-repeating-characters.ts
@@ -6,17 +6,34 @@
 
 // @lc code=start
 function lengthOfLongestSubstring(s: string): number {
+    // let max = 0;
+    // const sarr = s.split('');
+    // let temp = [] as string[];
+    // for (let i = 0; i < sarr.length; i++) {
+    //     if(temp.includes(sarr[i])) {
+    //         max = Math.max(max, temp.length);
+    //         temp = temp.slice(temp.indexOf(sarr[i]) + 1)
+    //     }
+    //     temp.push(sarr[i]);
+    // }
+    // return Math.max(max, temp.length);
+
+    // better solution
     let max = 0;
-    const sarr = s.split('');
-    let temp = [] as string[];
-    for (let i = 0; i < sarr.length; i++) {
-        if(temp.includes(sarr[i])) {
-            max = Math.max(max, temp.length);
-            temp = temp.slice(temp.indexOf(sarr[i]) + 1)
+    let window = new Set();
+    let left = 0;
+    let right = 0;
+    while (right < s.length) {
+        const char = s[right];
+        while (window.has(char)) {
+            window.delete(s[left]);
+            left++;
         }
-        temp.push(sarr[i]);
+        window.add(char);
+        max = Math.max(max, window.size);
+        right++;
     }
-    return Math.max(max, temp.length);
+    return max;
 };
 // @lc code=end
 
